86083988f28d24fdbb5bd8bd59663a8fa5cf990e
Rifat <rifat@setuptech.international>
2019-11-19 20:13:07 +0600

    Hold till profile picture and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage('https://tinyurl.com/yx6ba94d'),
+              ),
+              Text(
+                'Rifat',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
